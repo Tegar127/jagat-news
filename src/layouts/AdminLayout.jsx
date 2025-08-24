704769162c89b12000f1b67eb308067df31b75d9
Tegar127 <mtegarfitrianoj123@gmail.com>
2025-08-24 20:45:02 +0700

    bug

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -26,11 +26,11 @@ const AdminLayout = () => {
     }
 
     return (
-        <div className="flex h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+        <div className="flex h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300 overflow-hidden">
             <Sidebar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
             <div className="flex-1 flex flex-col overflow-hidden">
                 <Header setSidebarOpen={setSidebarOpen} />
-                <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 dark:bg-gray-900 p-4 md:p-6 lg:p-8 transition-all duration-300">
+                <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 dark:bg-gray-900 p-4 md:p-6 lg:p-8 pb-16 transition-all duration-300">
                     <div className="max-w-7xl mx-auto w-full animate-fadeIn">
                         <Outlet />
                     </div>
